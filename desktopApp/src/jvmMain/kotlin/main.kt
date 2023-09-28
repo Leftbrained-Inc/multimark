93d370990465e7f9a01c65b957166e836fe0b755
pank-su <frostkslo1@yandex.ru>
2023-09-28 13:31:33 +0300

    feat: add logo and title to window

--- desktopApp/src/jvmMain/kotlin/main.kt
+++ desktopApp/src/jvmMain/kotlin/main.kt
@@ -1,8 +1,9 @@
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(onCloseRequest = ::exitApplication, title = "Multimark", icon = painterResource("logo.svg")) {
         MainView()
     }
 }
