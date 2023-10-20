9ce082c34986408965f01228cde09f4e720a9a80
pank-su <frostkslo1@yandex.ru>
2023-10-20 10:31:39 +0300

    feat: add theming

---
+++ src/commonMain/kotlin/ui/theme/Theme.kt
@@ -0,0 +1,30 @@
+package ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorScheme = darkColorScheme(
+)
+
+private val LightColorScheme = lightColorScheme(
+)
+
+@Composable
+fun MultimarkAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true, content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
