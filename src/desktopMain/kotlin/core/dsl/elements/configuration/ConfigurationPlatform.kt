e179b679ff9e9f051db5cb706bdedce052e43a86
pank-su <frostkslo1@yandex.ru>
2023-10-25 10:51:40 +0300

    Исправление и добавление навигации

--- src/desktopMain/kotlin/core/dsl/elements/configuration/ConfigurationPlatform.kt
+++ src/desktopMain/kotlin/core/dsl/elements/configuration/ConfigurationPlatform.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.rememberWindowState
 import com.bumble.appyx.navigation.integration.DesktopNodeHost
+import core.dsl.elements.shortcut.shorts
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
