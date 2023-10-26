1871129339c8af7e4f589768b5d997831f05f033
Marat <marat18032005@mail.ru>
2023-10-26 12:16:35 +0300

    fix: import libs

--- src/commonMain/kotlin/ui/screen/StartScreen.kt
+++ src/commonMain/kotlin/ui/screen/StartScreen.kt
@@ -1,5 +1,8 @@
 package ui.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
@@ -10,6 +13,8 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
