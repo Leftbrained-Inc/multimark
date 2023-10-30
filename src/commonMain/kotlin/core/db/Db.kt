bd0e4b57f6782fd0d2b3d457e22da2fe4dcc540b
pank-su <frostkslo1@yandex.ru>
2023-10-30 13:32:31 +0300

    docs: some clears

--- src/commonMain/kotlin/core/db/Db.kt
+++ src/commonMain/kotlin/core/db/Db.kt
@@ -1,29 +1,15 @@
 package core.db
 
-import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.*
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
-import org.jetbrains.exposed.sql.transactions.transaction
 
 /**
- * Класс для работы с базой данных приложения
+ * Класс для работы с базой данных
  * @author Сергей Рейнн (bulkabuka)
+ * @author Панков Вася (pank-su)
  */
 class Db {
-
     companion object {
         val connect by lazy { Database.connect("jdbc:sqlite:main.db", "org.sqlite.JDBC") }
     }
-
-
-    init {
-
-    }
-
-    /**
-     * Класс для работы с закрепленными файлами в базе данных
-     * @author Сергей Рейнн (bulkabuka)
-     */
-
 }
 
