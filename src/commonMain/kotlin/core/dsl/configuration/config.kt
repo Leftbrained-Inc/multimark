8f04c94da76c648a5531a9c2a7d8db72e70eae93
pank-su <frostkslo1@yandex.ru>
2023-11-08 15:10:03 +0300

    feat & fix: разделение нативных компонентов от ненативных
    Теперь config вызывает внутри себя необходимые компоненты для инициализации, которые и нативные и не нативные, это помогает в том что пользователю теперь не надо вызывать команду render, всё делается само (вот это магия)

--- src/commonMain/kotlin/core/dsl/configuration/config.kt
+++ src/commonMain/kotlin/core/dsl/configuration/config.kt
@@ -1,13 +1,7 @@
 package core.dsl.configuration
 
-import core.configuration.ConfigurationImpl
-
 /**
  * DSL конфигурации
  * @author Панков Вася (pank-su)
  */
-fun config(init: ConfigurationBuilder.() -> Unit): ConfigurationImpl {
-    val config = ConfigurationBuilder()
-    config.init()
-    return config.build()
-}
+expect fun config(init: ConfigurationBuilder.() -> Unit)
--- src/desktopMain/kotlin/core/configuration/ConfigurationPlatform.desktop.kt
+++ src/desktopMain/kotlin/core/configuration/ConfigurationPlatform.desktop.kt
@@ -1,11 +1,10 @@
 package core.configuration
 
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.runtime.*
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import com.bumble.appyx.navigation.integration.DesktopNodeHost
 import core.extensions.keyMap
@@ -17,7 +16,6 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.flow.receiveAsFlow
 import navigation.RootNode
-import ui.utils.Scale
 
 
 sealed class Events {
@@ -31,42 +29,28 @@ sealed class Events {
  * @author Панков Вася (pank-su)
  */
 actual abstract class ConfigurationPlatform actual constructor() : core.configuration.Configuration() {
+
     /**
      * Отображение экрана и навигации
-     *
-     * @param onCloseRequest передаётся request на закрытие приложения
-     *
      * @author Панков Вася (pank-su)
      */
-    @Composable
-    actual open fun render(onCloseRequest: () -> Unit) {
-        val events: Channel<Events> = Channel()
-        val windowState = rememberWindowState(size = DpSize(480.dp, 658.dp))
-        val eventScope = remember { CoroutineScope(SupervisorJob() + Dispatchers.Main) }
-
-        val configuration = this
-
-        CompositionLocalProvider(LocalConfiguration provides configuration as ConfigurationImpl) {
-            Window(onCloseRequest, onKeyEvent = { keyEvent ->
-                configuration.keyMap.shorts.forEach {
-                    if (it.condition(keyEvent)) {
-                        it.action()
-                        return@Window true
+    actual open fun render() {
+        application {
+            val events: Channel<Events> = Channel()
+            val windowState = rememberWindowState(size = DpSize(480.dp, 658.dp))
+            val eventScope = remember { CoroutineScope(SupervisorJob() + Dispatchers.Main) }
+            content {
+                val configuration = LocalConfiguration.current
+                Window(this::exitApplication, onKeyEvent = { keyEvent ->
+                    configuration.keyMap.shorts.forEach {
+                        if (it.condition(keyEvent)) {
+                            it.action()
+                            return@Window true
+                        }
                     }
-                }
-
-                false
-            }, icon = this.window.icon, title = this.window.title) {
-
-                val config = LocalConfiguration.current
-
-                LaunchedEffect(config.scale) {
-                    Scale.scale = config.scale
-                    Scale.fontScale = config.fontScale
-                }
-
-                AnimatedContent(Scale.scale) {
-                    config.theme {
+                    false
+                }, icon = configuration.window.icon, title = configuration.window.title) {
+                    it {
                         DesktopNodeHost(
                             windowState = windowState,
                             onBackPressedEvents = events.receiveAsFlow().mapNotNull {
@@ -77,10 +61,7 @@ actual abstract class ConfigurationPlatform actual constructor() : core.configur
                         }
                     }
                 }
-
             }
         }
     }
-
-
 }
