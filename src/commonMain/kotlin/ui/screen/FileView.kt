8e1c5a4329e91f04bb7197ace5d68e176b2c8f90
pank-su <frostkslo1@yandex.ru>
2023-11-10 10:58:56 +0300

    fix: передача путя вместо Source

--- src/commonMain/kotlin/ui/screen/FileView.kt
+++ src/commonMain/kotlin/ui/screen/FileView.kt
@@ -2,11 +2,13 @@ package ui.screen
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import kotlinx.io.Source
+import kotlinx.io.buffered
+import kotlinx.io.files.Path
+import kotlinx.io.files.SystemFileSystem
 import kotlinx.io.readString
 
 @Composable
-fun FileView(file: Source){
-    val content = file.readString()
+fun FileView(path: Path) {
+    val content = SystemFileSystem.source(path).buffered().readString()
     Text(content)
 }
