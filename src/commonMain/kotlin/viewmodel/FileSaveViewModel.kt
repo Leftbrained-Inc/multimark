13407241576345f2dff4b6207a27ce955151cb64
Shizik <tchelnokov88@ya.ru>
2023-12-08 16:15:11 +0300

    docs: Добавлены комментарии

--- src/commonMain/kotlin/viewmodel/FileSaveViewModel.kt
+++ src/commonMain/kotlin/viewmodel/FileSaveViewModel.kt
@@ -7,13 +7,24 @@ import kotlinx.io.files.SystemFileSystem
 import kotlinx.io.readString
 import kotlinx.io.writeString
 
-class FileSaveViewModel(val path: Path){
+/**
+ * ViewModel функции сохронения изменений
+ * @property text переменная в которую заносится  текст
+ * @property contentText переменная в которую заносится изменённый текст
+ * @property isSaved переменная проверки на совподение сохронённого текста
+ * @author Алексей Челноков (shizik-tech)
+ */
+
+class FileSaveViewModel(val path: Path) {
 
     private var contentText by mutableStateOf("")
 
     var text by mutableStateOf("")
     val isSaved by derivedStateOf { contentText == text }
 
+    /**
+     * Чтение файла
+     */
     init {
         val buffer = SystemFileSystem.source(path).buffered()
         text = buffer.readString()
@@ -21,7 +32,10 @@ class FileSaveViewModel(val path: Path){
         buffer.close()
     }
 
-    fun saveFile(){
+    /**
+     * Функция сохранения
+     * */
+    fun saveFile() {
         val sink = SystemFileSystem.sink(path).buffered()
 
         sink.writeString(text)
