f207dd8c4e55b3abbacf05becc62161160dda6d0
Shizik-tech <tchelnokov88@ya.ru>
2023-12-09 15:33:36 +0300

    docs: Добавлена документация

--- src/commonMain/kotlin/ui/components/TabCategory.kt
+++ src/commonMain/kotlin/ui/components/TabCategory.kt
@@ -10,8 +10,19 @@ import androidx.compose.ui.input.key.onPreviewKeyEvent
 import kotlinx.io.files.Path
 import viewmodel.EditViewModel
 
+/**
+ * Категории вкладок
+ * @author Сергей Рейнн (bulkabuka)
+ * @author Василий Панков (pank-su)
+ * @param name название вкладки
+ * @param screen открытый экран
+ * */
 sealed class TabCategory(val name: String, var screen: @Composable (TabCategory) -> Unit = { }) {
-
+/**
+ * Вкладка редактирования файла
+ * @param path путь файла
+ * @param viewModel viemodel класса редактора
+ * */
     data class Edit(val path: Path, private val viewModel: EditViewModel = EditViewModel(path)) :
         TabCategory(path.name){
             val isSaved by derivedStateOf { viewModel.isSaved }
@@ -33,9 +44,16 @@ sealed class TabCategory(val name: String, var screen: @Composable (TabCategory)
                 }
             }
         }
-
+/**
+ * TODO
+ * Дерево файлов
+ * */
     data class Tree(val path: Path) : TabCategory("Tree")
     data class View(val path: Path) : TabCategory("View")
+
+    /**
+     * Пустая вкладка для тестирования
+     */
     data object Empty : TabCategory("Empty")
 
 
