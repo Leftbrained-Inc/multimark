cdc9d1ef7ff796aa57620c3b841161c6c85a989e
pank-su <frostkslo1@yandex.ru>
2024-02-07 10:58:07 +0300

    change logo
    Signed-off-by: pank-su <frostkslo1@yandex.ru>

--- demos/desktop_test/src/main/kotlin/Main.kt
+++ demos/desktop_test/src/main/kotlin/Main.kt
@@ -1,8 +1,17 @@
+import androidx.compose.foundation.Image
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import core.dsl.configuration.config
 
 fun main() {
     config {
-        scale = 2f
+        logo {
+            Image(
+                painterResource("Good Tick.svg"), null, modifier = it,
+                contentScale = ContentScale.FillHeight
+            )
+        }
+        scale = 1f
         fontScale = 1f
     }
 }
