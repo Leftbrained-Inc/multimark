c7869078accc714a9a12d42d5ce427500b14ad3f
pank-su <frostkslo1@yandex.ru>
2024-02-07 11:55:41 +0300

    keymap config
    Signed-off-by: pank-su <frostkslo1@yandex.ru>

--- demos/desktop_test/src/main/kotlin/Main.kt
+++ demos/desktop_test/src/main/kotlin/Main.kt
@@ -1,8 +1,15 @@
 import androidx.compose.foundation.Image
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.isAltPressed
+import androidx.compose.ui.input.key.isCtrlPressed
+import androidx.compose.ui.input.key.key
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import core.configuration.FilesMode
 import core.dsl.configuration.config
+import core.dsl.elements.shortcut.Shortcut
+import core.extensions.KeyMap
+import core.shortcut.keyMap
 
 fun main() {
     config {
@@ -15,6 +22,13 @@ fun main() {
 
         scale = 1f
         fontScale = 1f
+
+        keyMap {
+            KeyMap.metaKey = { it.isCtrlPressed }
+            testShortcut = Shortcut({ keyEvent -> keyEvent.isAltPressed && keyEvent.key == Key.K }) {
+                println("Test")
+            }
+        }
 
         launchScreen {
             filesMode = FilesMode.Both
