c68ce3e2220a6c8409a0296919ef08deae3d9c75
pank-su <frostkslo1@yandex.ru>
2024-02-07 11:57:00 +0300

    window config
    Signed-off-by: pank-su <frostkslo1@yandex.ru>

--- demos/desktop_test/src/main/kotlin/Main.kt
+++ demos/desktop_test/src/main/kotlin/Main.kt
@@ -4,11 +4,15 @@ import androidx.compose.ui.input.key.isAltPressed
 import androidx.compose.ui.input.key.isCtrlPressed
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.loadSvgPainter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.useResource
+import androidx.compose.ui.unit.Density
 import core.configuration.FilesMode
 import core.dsl.configuration.config
 import core.dsl.elements.shortcut.Shortcut
 import core.extensions.KeyMap
+import core.extensions.window.window
 import core.shortcut.keyMap
 
 fun main() {
@@ -28,6 +32,11 @@ fun main() {
             testShortcut = Shortcut({ keyEvent -> keyEvent.isAltPressed && keyEvent.key == Key.K }) {
                 println("Test")
             }
+        }
+
+        window {
+            icon = useResource("Good Tick.svg") { loadSvgPainter(it, Density(100f)) }
+            title = "Good editor"
         }
 
         launchScreen {
