f60c608fc4d778551b77cb012881c2589a2f550e
pank-su <frostkslo1@yandex.ru>
2024-02-07 10:54:41 +0300

    empty config
    Signed-off-by: pank-su <frostkslo1@yandex.ru>

--- demos/desktop_test/src/main/kotlin/Main.kt
+++ demos/desktop_test/src/main/kotlin/Main.kt
@@ -1,42 +1,7 @@
-import androidx.compose.ui.input.key.isAltPressed
-import androidx.compose.ui.input.key.isCtrlPressed
-import androidx.compose.ui.res.loadSvgPainter
-import androidx.compose.ui.res.useResource
-import androidx.compose.ui.unit.Density
-import core.configuration.FilesMode
 import core.dsl.configuration.config
-import core.dsl.elements.shortcut.Shortcut
-import core.extensions.KeyMap
-import core.extensions.window.window
-import core.shortcut.keyMap
 
 fun main() {
     config {
-//        logo {
-//            Image(painterResource("Good Tick.svg"), null, modifier = it, contentScale = ContentScale.FillHeight)
-//        }
 
-        scale = 1f
-        fontScale = 1f
-
-        window {
-            icon = useResource("Good Tick.svg") { loadSvgPainter(it, Density(100f)) }
-            title = "Test"
-        }
-
-        keyMap {
-            KeyMap.metaKey = { it.isCtrlPressed }
-            testShortcut = Shortcut({ keyEvent -> keyEvent.isAltPressed }) {
-                println("Test")
-            }
-        }
-
-        launchScreen {
-            filesMode = FilesMode.Both
-
-            recentFilter {
-                true
-            }
-        }
     }
 }
